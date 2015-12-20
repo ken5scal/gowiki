036d653ef9c7be17bc153eb34fcdcd04ef8af855
ken5scal <kengoscal@gmail.com>
2015-12-20 23:21:07 +0900

    Add editWikiHandler method

--- wiki.go
+++ wiki.go
@@ -27,6 +27,22 @@ func viewWikiHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 }
 
+// loads the page (if it the page doesn't exist, create an empty Page struct
+func editWikiHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
+}
+
 /*
   Data Structures
 */
